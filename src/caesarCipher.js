a000a463384199dd0acf43f31774536bd1002ad8
Chivu Andrei <chivuandrei2003@gmail.com>
2025-09-03 18:57:40 +0300

    small update to caesarCipher function

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,5 +1,15 @@
-function caesarCipher(string ,shifts) {
-    
+//https://stackoverflow.com/questions/10617123/associative-array-with-alphabets-as-keys-in-javascript
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+console.log(alphabet[0]);
+/*
+let transformVal = () => {
+    for (let i = 0; i < transformVal.length; i++){
+        transformVal[i] = ;
+    }
+}*/
+function caesarCipher(val, shifts) {
+    return String(val)
 }
 
 module.exports = caesarCipher;
